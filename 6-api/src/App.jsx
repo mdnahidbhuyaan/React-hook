e2378bafd540db3aaed69b69dfc2407e23ec3e1b
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-12-19 12:30:23 +0600

    useEffect

--- 6-api/src/App.jsx
+++ 6-api/src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 const App = () => {
+  const [a , setA] =useState(0);
+  const [b , setB] =useState(0);
+  
+  function aChanging(){
+    console.log("A ke value change hocca");
+    
+  }
+    function bChanging(){
+    console.log("B ke value change hocca");
+    
+  }
+
+
+  useEffect(()=>{
+    bChanging();
+  },[b])
+
+
+
   return (
     <div>
-      Aa
+      <h1>A is {a}</h1>
+      <h1>B is {b}</h1>
+      <button onClick={()=>{
+        setA(a+1);
+      }}>Change A</button>
+      <button onClick={()=>{
+        setB(b-1);
+      }}>Change B</button>
     </div>
   )
 }
