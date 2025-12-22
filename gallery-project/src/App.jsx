b1e80afd7f257911f1b3da318b21fcb81bf58cd6
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-12-22 22:42:04 +0600

    gallery

--- gallery-project/src/App.jsx
+++ gallery-project/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+import React, { useState } from "react";
+
+const App = () => {
+  const [userData, setUserData] = useState([]);
+
+  const getdata = async () => {
+    const response = await axios.get(
+      "https://picsum.photos/v2/list?page=2&limit=10"
+    );
+    setUserData(response.data);
+    console.log(response.data);
+ 
+  };
+
+
+  let prientUserData = "No user available";
+
+  if(userData.length){
+    prientUserData = userData.map(function(elem, idx){
+      return <div>
+        <img className="h-40 " src={elem.download_url} alt="" />
+      </div>
+    })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="p-10 h-screen overflow-auto">
+      <button
+        onClick={getdata}
+        className="bg-cyan-400 rounded-sm px-5 py-2 active:scale-95 text-white"
+      >
+        Click
+      </button>
+      <div>{prientUserData}</div>
+    </div>
+  );
+};
 
-export default App
+export default App;
